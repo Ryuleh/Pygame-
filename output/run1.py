0f28aeaaeea1f939e8e194bb6d67eccfbba573ab
Hassan Zafar <hassaanzafar3@gmail.com>
2023-08-22 02:25:02 -0400

    first commit

---
+++ output/run1.py
@@ -0,0 +1,40 @@
+import pygame
+from sys import exit
+from random import randint, choice
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        player_walk_1 = pygame.image.load("player1").convert_alpha()
+        player_walk_2 = pygame.image.load("player2").convert_alpha()
+        self.player_walk = [player_walk_1, player_walk_2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load("Jump").convert_alpha()
+
+        self.gravity = 0
+        self.image = self.player_walk[self.player_index]
+        self.rect = self.image.get_rect(midbottom=(50, 310))
+        self.player_jump_sound = pygame.mixer.Sound()
+        self.player_jump_sound.set_volume(0.5)
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom > 300:
+            self.gravity -= 20
+            self.player_jump_sound.play()
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 300:
+            self.rect.bottom = 300
+
+    def animation_state(self):
+        if self.rect.bottom < 300:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
---
+++ output/weather.py
@@ -0,0 +1,98 @@
+import tkinter as tk
+from tkinter import messagebox
+
+window = tk.Tk()
+window.title("Weather App")
+window.geometry("778x400")
+window.configure(bg="lightblue")
+
+
+def verify_login():
+    username = username_entry.get()
+    password = password_entry.get()
+
+    with open("logins.txt", "r") as logins:
+        for line in logins:
+            stored_username, stored_password = line.strip().split(",")
+
+            if username == stored_username and password == stored_password:
+                messagebox.showinfo(
+                    "Login Successful", "You have successfully logged in!"
+                )
+                return
+    messagebox.showerror("Invalid Login", "Username or Password is Incorrect!")
+
+
+def new_user():
+    username = username_entry.get()
+    password = password_entry.get()
+
+    with open("logins.txt", "r") as logins:
+        content = logins.read()
+
+    if username in content or password in content:
+        messagebox.showerror("Invalid Login", "Username or Password is Incorrect!")
+
+    else:
+        logins.create()
+        logins.write(f"{username},{password}\n")
+        messagebox.showinfo(
+            "New User Created", "You have successfully created a new user!"
+        )
+
+
+username_label = tk.Label(
+    window, text="Username", font=("Arial", 14), fg="darkgreen", bg="lightblue"
+)
+username_label.grid(row=0, column=0)
+username_entry = tk.Entry(window, font=("Arial", 14))
+username_entry.grid(row=0, column=1)
+
+password_label = tk.Label(
+    window, text="Password", font=("Arial", 14), fg="darkgreen", bg="lightblue"
+)
+password_label.grid(row=1, column=0)
+password_entry = tk.Entry(window, show="*", font=("Arial", 14))
+password_entry.grid(row=1, column=1)
+
+login_button = tk.Button(
+    window,
+    text="Login",
+    command=verify_login,
+    width=10,
+    height=2,
+    bg="lightgreen",
+    fg="black",
+)
+login_button.grid(row=4, column=1)
+
+new_user_button = tk.Button(
+    window,
+    text="New User",
+    command=new_user,
+    width=10,
+    height=2,
+    bg="lightgreen",
+    fg="black",
+)
+new_user_button.grid(row=4, column=0)
+
+
+def login(self):
+    self.root.destroy()
+    main_window = tk.Tk()
+    main_window.title("Weather App - Main Window")
+    main_window.geometry("800x400")
+    main_window.configure(bg="lightblue")
+
+    city_label = tk.Entry(
+        main_window,
+        text="Enter City:",
+        font=("Arial", 14),
+        fg="darkgreen",
+        bg="lightblue",
+    )
+    city_label.pack(pady=10)
+
+
+window.mainloop()
