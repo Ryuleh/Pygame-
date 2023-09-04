aa95aa1db808e880a669e8841f16893dfab2a582
Ryuleh <92819040+Ryuleh@users.noreply.github.com>
2023-09-04 10:48:16 -0400

    yhgfd

--- venv/app.py
+++ venv/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask , render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-  
+def hello_world():
+    name = request.args.get("name")
+    return render_template('index.html', name=name)
+if __name__ == '__main__':
+    app.run()
